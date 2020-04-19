1e13a24a06edd266107bb1b83169db83f97615ee
Kenals <kenals01@gmail.com>
2020-04-19 19:17:33 +0100

    commit

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 function bottleUp(number){
-let output= [];
-if(!isNaN(number)){
-for (let i = 1; i<=number; i++){
-if(i %2 ===0 && i %3 ===0 && i%5 ===0){
-    output.push("yu-gi-oh");
-}else if (i %2 ===0 && i %3 ===0){
-   output.push("yu-gi");
-}else if(i %2 ===0 && i %5 ===0){
- output.push("yu-oh");
-}else if (i %3 ===0 && i %5 ===0){
-   output.push("gi-oh");
-}else if (i %2 ===0){
-  output.push("yu");
-}else if (i %3 ===0){ 
-    output.push("gi");
-}else if (i %5 ===0){
-    output.push("oh");
-}else
- {output.push(i);
-}
-}
-}
-console.log(output);
-return output;
-}
-bottleUp(100);
-bottleUp(15);
+    let output= [];
+    if(!isNaN(number)){
+    for (let i = 1; i<=number; i++){
+    if(i %2 ===0 && i %3 ===0 && i%5 ===0){
+        output.push("yu-gi-oh");
+    }else if (i %2 ===0 && i %3 ===0){
+       output.push("yu-gi");
+    }else if(i %2 ===0 && i %5 ===0){
+     output.push("yu-oh");
+    }else if (i %3 ===0 && i %5 ===0){
+       output.push("gi-oh");
+    }else if (i %2 ===0){
+      output.push("yu");
+    }else if (i %3 ===0){ 
+        output.push("gi");
+    }else if (i %5 ===0){
+        output.push("oh");
+    }else
+     {output.push(i);
+    }
+    }
+    console.log(output);
+    }else{
+    output.push(number + "is not a number")
+    }
+    console.log(output);
+    return output;
+    }
+    bottleUp(100);
+    bottleUp(15);
