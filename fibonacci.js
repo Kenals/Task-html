23a0b21a6136928afb2d013033d9b480eca6daa8
Kenals <kenals01@gmail.com>
2020-05-02 22:37:49 +0100

    commit

--- fibonacci.js
+++ fibonacci.js
@@ -1,18 +1,19 @@
-let fib = function(input, output) {
-let a = input[0],
-    b = input[1],
-    next,
-    cnt =  2;
-    
-    while (cnt < output) {
-next = a + b;
-a = b;
-b = next;
-input.push(next);
-cnt++;
-}
-return input;
-}
-fib([0, 1], 20);
-
-
+let fib = function(output) {
+    let input = [];
+     let a = 0;
+     let  b = 1;
+     let next = a+b;
+     input.push(a,b);
+         index =  2;
+           
+     while (index <output) {
+       next = a + b;
+       a = b;
+       b = next;
+       input.push(next);
+       index++;
+ }   
+  return input;
+ }
+   fib(20);
+       
